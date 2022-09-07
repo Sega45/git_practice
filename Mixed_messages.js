517515a90b866d5ddde8b8a6c501d406bc4a2aa6
Sega <msk234432@gmail.com>
2022-09-07 18:36:16 +0200

    Change name

---
+++ Mixed_messages.js
@@ -0,0 +1,21 @@
+const signs = ['Pisces', 'Aquarius', 'Capricorn'];
+
+const messagePisces = 'You could experience chimerical chemistry in a dance of mutual seduction.';
+const messageAquarius = 'Experiment beyond your everyday routine so you cross paths with unfamiliar faces.';
+const messageCapricorn = 'As the moon in your resourceful second house clicks with clever Mercury in your ambitious tenth house, keep moving in the right direction, inch by inch, and you’ll get to the goal line faster than you expect.';
+
+const chooseSign = () => {
+    const number = Math.floor(Math.random() * 2);
+    switch(number) {
+        case 0:
+            console.log(`Your sign is ${signs[0]}. Your prognose for today: ${messagePisces}`);
+            break;
+        case 1:
+            console.log(`Your sign is ${signs[1]}. Your prognose for today: ${messageAquarius}`);
+            break;
+        case 2:
+            console.log(`Your sign is ${signs[2]}. Your prognose for today: ${messageCapricorn}`);
+            break;
+    }
+};
+chooseSign();
